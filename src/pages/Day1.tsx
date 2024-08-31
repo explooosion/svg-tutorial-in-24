eb9a33e19d70e3fb8ec5bbb9792c4145dd44811e
Robby Wu <ta7382@gmail.com>
2024-09-01 00:04:56 +0800

    chore: Add Day1 component with SVG shape

---
+++ src/pages/Day1.tsx
@@ -0,0 +1,18 @@
+function Day1() {
+  return (
+    <svg width="200" height="200" viewBox="-100 -100 200 200">
+      <circle
+        cx="0"
+        cy="-75"
+        r="12"
+        fill="none"
+        stroke="#f79257"
+        strokeWidth="2"
+      />
+      <rect x="-17.5" y="-65" width="35" height="20" fill="#f79257" />
+      <circle cx="0" cy="20" r="70" fill="#d1495b" />
+    </svg>
+  );
+}
+
+export default Day1;
