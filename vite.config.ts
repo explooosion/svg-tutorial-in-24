f5e4706122d0f1f5417869244aa4a43e412f7a65
Robby Wu <ta7382@gmail.com>
2024-09-01 15:03:10 +0800

    chore: Update vite.config.ts for production build and GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === "production" ? "/svg-tutorial-in-24/" : "/",
+    build: {
+      outDir: "docs",
+    },
+  };
+});
